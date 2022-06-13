3f48ac2f82bb3d1c434c7386bd5bc7cd6451e0ca
theunissensjg <theunissensjg@gmail.com>
2022-06-13 10:15:23 +0200

    change folder structure

---
+++ sections/section5.sql
@@ -0,0 +1,15 @@
+-- FROM, SELECT, ORDER BY
+-- you can ORDER BY alias
+
+-- SELECT date_of_birth, first_name ||' '|| last_name AS full_name
+-- FROM movies.actors
+-- ORDER BY date_of_birth ASC;
+
+-- SELECT date_of_birth, first_name ||' '|| last_name AS full_name
+-- FROM movies.actors
+-- ORDER BY full_name ASC;
+
+-- SELECT date_of_birth, LENGTH(last_name) AS length_last_name
+-- FROM movies.actors
+-- ORDER BY length_last_name DESC;
+
